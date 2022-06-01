347af4fc1bd0c493503974b436c0a386cfc29035
Felipe Reis <51302114+lipecoding@users.noreply.github.com>
2022-05-31 22:42:59 -0300

    fix db error

--- src/CRUD/cliente/teste.java
+++ src/CRUD/cliente/teste.java
@@ -4,43 +4,44 @@ import java.util.Scanner;
 
 public class teste{
     public static void main(String[] args) {
+        
+
         Scanner leitor = new Scanner(System.in);
-        Cliente cliente = new Cliente();
+        Cliente objCliente = new Cliente();
+        clienteCon objClienteCon = new clienteCon();        
 
         System.out.println("========================\nCadastro de cliente\n========================= ");
         System.out.println();
 
         System.out.println("Digite seu nome");
-        cliente.setNome(leitor.next());
+        objCliente.setNome(leitor.next());
 
         System.out.println("Digite seu sobrenome");
         leitor.nextLine();
-        cliente.setSobrenome(leitor.nextLine());
+        objCliente.setSobrenome(leitor.nextLine());
 
         System.out.println("Digite sua idade");
-        cliente.setIdade(leitor.nextInt());
+        objCliente.setIdade(leitor.nextInt());
 
         System.out.println("Digite seu CPF");
-        cliente.setCpf(leitor.next());
+        objCliente.setCpf(leitor.next());
 
         System.out.println("Digite seu endereco");
         leitor.nextLine();
-        cliente.setEndereco(leitor.nextLine());
+        objCliente.setEndereco(leitor.nextLine());
 
         System.out.println("Digite seu CEP");
-        cliente.setCep(leitor.next());
+        objCliente.setCep(leitor.next());
 
         System.out.println("Digite seu e-mail");
-        cliente.setEmail(leitor.next());
+        objCliente.setEmail(leitor.next());
 
         System.out.println("Digite sua senha(apenas numeros)");
-        cliente.setSenha(leitor.nextInt());
+        objCliente.setSenha(leitor.nextInt());
 
         System.out.println("Digite seu telefone");
-        cliente.setTelefone(leitor.next());
+        objCliente.setTelefone(leitor.next());
 
-        Cliente objCliente = new Cliente();
-        clienteCon objClienteCon = new clienteCon();
         objClienteCon.cadastroCliente(objCliente);
     }    
 }
